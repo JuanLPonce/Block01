eaa9daa93da87c9890616e5b6d2b1966fc14797c
JuanLPonce <63964576+JuanLPonce@users.noreply.github.com>
2020-04-25 23:08:27 +0200

    Add files via upload

---
+++ Exercise6/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d, e;
+            double SUM;
+           
+            
+
+            Console.WriteLine("Write five integers and I will do the sum: ");
+
+            a = Convert.ToDouble(Console.ReadLine());
+            
+            if (a % 2 == 0 || a % 2 == 1)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("You didn't introduced an integer, please try again: ");
+                a = Convert.ToDouble(Console.ReadLine());
+            }
+
+            b = Convert.ToDouble(Console.ReadLine());
+            
+            if (b % 2 == 0 || b % 2 == 1)
+            {
+               
+            }
+            else
+            {
+                Console.WriteLine("You didn't introduced an integer, please try again: ");
+                b = Convert.ToDouble(Console.ReadLine());
+            }
+
+            c = Convert.ToDouble(Console.ReadLine());
+
+            if (c % 2 == 0 || c % 2 == 1)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("You didn't introduced an integer, please try again: ");
+                c = Convert.ToDouble(Console.ReadLine());
+            }
+
+            d = Convert.ToDouble(Console.ReadLine());
+
+            if (d % 2 == 0 || d % 2 == 1)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("You didn't introduced an integer, please try again: ");
+                d = Convert.ToDouble(Console.ReadLine());
+            }
+
+            e = Convert.ToDouble(Console.ReadLine());
+            
+            if (e % 2 == 0 || e % 2 == 1)
+            {
+               
+            }
+            else
+            {
+                Console.WriteLine("You didn't introduced an integer, please try again: ");
+                e = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine("These are the numbers you have chosen: ");
+            Console.WriteLine("First number : " + a);
+            Console.WriteLine("Second number : " + b);
+            Console.WriteLine("Third number : " + c);
+            Console.WriteLine("Fourth number : " + d);
+            Console.WriteLine("Fifth number : " + e);
+
+            SUM = a + b + c + d + e;
+
+            Console.WriteLine("The sum is: " + SUM);
+        } 
+    } 
+}
---
+++ Exercise8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Introduce four digits: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            int a = num % 10;
+            int b = (num / 10) % 10;
+            int c = (num / 100) % 10;
+            int d = (num / 1000) % 10;
+
+            Console.WriteLine("This is the number that you introduced: " + d + c + b + a);
+
+
+            int sum = a + b + c + d;
+            Console.WriteLine("This is the sum of the four digits: " + sum);
+
+            Console.WriteLine("This is the number in reverse order: " +a + b + c + d);
+
+            Console.WriteLine("This is the number with the last digit in first position:" +a +d +c +b);
+
+            Console.WriteLine("This is the exchange between the second and the third digit: " +d +b +c +a);
+        }
+    }
+}
